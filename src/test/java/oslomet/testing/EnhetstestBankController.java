104ac6641465b4a961f92e5c82943422e0453409
Twitchbisharp <s364544@oslomet.no>
2023-01-24 14:33:16 +0100

    Initial commit

---
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -0,0 +1,146 @@
+package oslomet.testing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import oslomet.testing.API.BankController;
+import oslomet.testing.DataAaccessLayer.BankRepository;
+import oslomet.testing.Models.Konto;
+import oslomet.testing.Models.Kunde;
+import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnhetstestBankController {
+
+    @InjectMocks
+    // denne skal testes
+    private BankController bankController;
+
+    @Mock
+    // denne skal Mock'es
+    private BankRepository repository;
+
+    @Mock
+    // denne skal Mock'es
+    private Sikkerhet sjekk;
+
+    @Test
+    public void hentKundeInfo_loggetInn() {
+
+        // arrange
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        when(repository.hentKundeInfo(anyString())).thenReturn(enKunde);
+
+        // act
+        Kunde resultat = bankController.hentKundeInfo();
+
+        // assert
+        assertEquals(enKunde, resultat);
+    }
+
+    @Test
+    public void hentKundeInfo_IkkeloggetInn() {
+
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        //act
+        Kunde resultat = bankController.hentKundeInfo();
+
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void hentKonti_LoggetInn()  {
+        // arrange
+        List<Konto> konti = new ArrayList<>();
+        Konto konto1 = new Konto("105010123456", "01010110523",
+                720, "Lønnskonto", "NOK", null);
+        Konto konto2 = new Konto("105010123456", "12345678901",
+                1000, "Lønnskonto", "NOK", null);
+        konti.add(konto1);
+        konti.add(konto2);
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        when(repository.hentKonti(anyString())).thenReturn(konti);
+
+        // act
+        List<Konto> resultat = bankController.hentKonti();
+
+        // assert
+        assertEquals(konti, resultat);
+    }
+
+    @Test
+    public void hentKonti_IkkeLoggetInn()  {
+        // arrange
+
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        List<Konto> resultat = bankController.hentKonti();
+
+        // assert
+        assertNull(resultat);
+    }
+
+
+    //De som kommer under her har jeg laget selv:
+    @Test
+    public void hentTransaksjoner_LoggetInn(){
+        //arrange
+        /* Trodde man måtte lage en fiktiv transaksjon også, men det måtte man ikke
+        List<Transaksjon> transaksjonList = new ArrayList<>();
+        Transaksjon betaling1 = new Transaksjon(1, "2514 23 1648", 200, "", "Melding heihei", "Mamma", "1548 54 5656");
+        transaksjonList.add(betaling1);
+        */
+        Konto konto1 = new Konto("12345678901", "1548 54 5656", 5000, "Privat kontro", "NOK", null);
+
+
+        when(sjekk.loggetInn()).thenReturn("12345678901");
+        when(repository.hentTransaksjoner(anyString(), anyString(), anyString())).thenReturn(konto1);
+
+        //act
+        Konto resultat = bankController.hentTransaksjoner("1548 54 5656", "", "");
+
+        //assert
+        assertEquals(konto1, resultat);
+    }
+
+    @Test
+    public void hentTransaksjoner_IkkeLoggetInn(){
+        //arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        //act - har egt. ikke noe å si siden vi ikke bruker noen av parameterne...
+        Konto resultat = bankController.hentTransaksjoner("", "", "");
+
+        //assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void hentSaldi_LoggetInn(){
+        //arrange
+
+    }
+}
+
